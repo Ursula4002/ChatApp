c2afe9c709b20981a42037f3ce6fb03090224c21
Ursula Ngalubenge <ursulangalubenge2021@gmail.com>
2026-07-18 17:27:52 +0100

    fix: fix missing keys  issue

---
+++ script/config.js
@@ -0,0 +1,11 @@
+export const config = {
+    // Configuration de l'API
+    API_BASE_URL: "https://kadea-chat-api.onrender.com",
+    API_KEY: "wksp_c7f8367d338d051ae4fbfe357909497a",
+    // Abstract (email verification)
+    ABSTRACT_EMAIL_KEY: "1f0fc9779e9843848e07a361a39d8d70",
+    
+    // Cloudinary
+    CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/obd42wdt/image/upload",
+    CLOUDINARY_PRESET: "ih4yfg4m"
+};
